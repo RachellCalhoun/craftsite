f4168b9cec511b82d1283594e0cb1ad59964bc2f
rachell <rachell.calhoun@gmail.com>
2016-02-13 16:42:52 +0900

    pip installed postman

--- craftsite/settings.py
+++ craftsite/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pagination',
+    #'pagination',
     'crafts',
     'accounts',
     'postman',
