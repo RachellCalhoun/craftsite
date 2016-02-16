5c0becd56ab24842f0950a9b28535803f548ba43
Hassan Abid <hassanabidpk89@gmail.com>
2016-02-16 15:33:04 +0900

    Add staticfile directory for collecstatic command

--- craftsite/settings.py
+++ craftsite/settings.py
@@ -115,5 +115,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 MEDIA_URL = '/uploads/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
