db7cc946f31e2366a4ff8d374f65d2fd1f86a157
rachell <rachell.calhoun@gmail.com>
2016-02-24 17:15:30 +0900

    added unread count to base, added max w/h for profile imgs

--- craftsite/settings.py
+++ craftsite/settings.py
@@ -12,11 +12,12 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-# from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS
+from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS
 
-# TEMPLATE_CONTEXT_PROCESSORS += (
-#     'django.core.context_processors.request',
-# )
+TEMPLATE_CONTEXT_PROCESSORS += (
+    'django.core.context_processors.request',
+    'postman.context_processors.inbox',
+)
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
